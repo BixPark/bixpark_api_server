d492881e55d338c57dad313b8a3da105b5fe43d8
Dewmal <dewmalnilanka@gmail.com>
2020-06-01 22:20:08 +0530

    Do a minor change for test build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -1,7 +1,10 @@
 mkdir _build
+mkdir _build/media
+#cp config.yaml _build/app.config.yaml
 go build -o _build
 cd spa_web
 npm install
+rm -R buid
 npm react-scripts build
 cd ../
 chmod +x ./_build/bixpark_server
--- spa_web/src/editor/auth/Login.js
+++ spa_web/src/editor/auth/Login.js
@@ -33,54 +33,57 @@ function Login() {
     });
 
     return (
-        <form onSubmit={formik.handleSubmit} className="w-full max-w-5xl w">
-            <h3 className="text-3xl">Login page</h3>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                           htmlFor="inline-full-name">
-                        Full Name
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input
-                        name={"username"}
-                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        id="username" type="text"
-                        onChange={formik.handleChange}
-                        value={formik.values.username}
+        <>
+            <h3 className="text-3xl">Sign In</h3>
+            <form onSubmit={formik.handleSubmit} className="w-full max-w-5xl w">
 
-                    />
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                               htmlFor="inline-full-name">
+                            Full Name
+                        </label>
+                    </div>
+                    <div className="md:w-2/3">
+                        <input
+                            name={"username"}
+                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            id="username" type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.username}
+
+                        />
+                    </div>
                 </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                           htmlFor="password">
-                        Password
-                    </label>
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                               htmlFor="password">
+                            Password
+                        </label>
+                    </div>
+                    <div className="md:w-2/3">
+                        <input
+                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            name={"password"}
+                            id="password" type="password" placeholder="******************"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
+                    </div>
                 </div>
-                <div className="md:w-2/3">
-                    <input
-                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        name={"password"}
-                        id="password" type="password" placeholder="******************"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                    />
+                <div className="md:flex md:items-center">
+                    <div className="md:w-1/3"></div>
+                    <div className="md:w-2/3">
+                        <button
+                            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                            type="submit">
+                            Login
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className="md:flex md:items-center">
-                <div className="md:w-1/3"></div>
-                <div className="md:w-2/3">
-                    <button
-                        className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                        type="submit">
-                        Login
-                    </button>
-                </div>
-            </div>
-        </form>
+            </form>
+        </>
     );
 }
 
